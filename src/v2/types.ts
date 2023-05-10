aee691d1f3e9dfda4da1bf54adbd0efe28e1a661
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-11 05:16:45 +0900

    refact: local state는 일단 없는 걸로

--- src/v2/types.ts
+++ src/v2/types.ts
@@ -3,14 +3,9 @@ import { MutableRefObject } from "react";
 /* TODO: AvailableHTMLElment 가짓수 늘리기 */
 export type AvailableHTMLElement = HTMLDivElement;
 
-export type ScrollApis<
-  GlobalInterface extends Record<string, any>,
-  // LocalInterface extends Record<string, any>,
-> = {
+export type ScrollApis<GlobalInterface extends Record<string, any>> = {
   getGlobal: GlobalInterface;
   setGlobal: (partial: Partial<GlobalInterface>) => void;
-  // getLocal: LocalInterface;
-  // setLocal: (partial: Partial<LocalInterface>) => void;
 };
 
 export type ScrollCallback<T extends Record<string, any>> = (
@@ -29,10 +24,9 @@ export type DefaultState<T extends Record<string, any>> = {
   defaultCallback?: ScrollCallback<T>;
 };
 
-export type ScrollHook<
-  T extends Record<string, any>,
-  // U extends Record<string, any>,
-> = (callback?: ScrollCallback<T>) => {
+export type ScrollHook<T extends Record<string, any>> = (
+  callback?: ScrollCallback<T>,
+) => {
   targetRef: MutableRefObject<AvailableHTMLElement | null>;
   globalState: T;
 };

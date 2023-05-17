4f6afca1d809cf263169c47d44567f3e5e5ef8fd
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-17 23:29:36 +0900

    Feat: 가로 스크롤 테스트 환경 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
 import "./App.css";
-import Red from "./components/Red";
-import Blue from "./components/Blue";
-import Global from "./components/Global";
+
 import { useEffect, useRef } from "react";
 import { useNestedScroll, useTestScroll } from "./components/hooks";
-import { Simulate } from "react-dom/test-utils";
 
 function App() {
   const ref1 = useRef<HTMLDivElement>(null);
   const ref2 = useRef<HTMLDivElement>(null);
 
-  const { targetRef, state: globalState } = useTestScroll();
-  const { targetRef: nestedRef, state: nestedState } = useNestedScroll();
-
-  /*
-    useEffect(() => {
-      if (ref1.current && ref2.current) {
-        ref1.current.addEventListener("scroll", (e) => {
-          if (ref1.current) console.log(ref1.current.scrollTop);
-        });
-      }
-    }, [ref1, ref2]);
-     */
+  const { targetRef, state: globalState } = useTestScroll({});
+  const { targetRef: nestedRef, state: nestedState } = useNestedScroll({});
 
   return (
     <div className="App">
-      {/*
-      <Red />
-      <Blue />
-      <Global />
-      */}
       <div className="progress">
         whole: {globalState.progress} nest: {nestedState.progress}
+      </div>
+      <div className="parentHorizontal">
+        <div className="childHorizontal">
+          <div className="miniHorizontal" />
+        </div>
       </div>
       <div className="parent" ref={targetRef}>
         parent
